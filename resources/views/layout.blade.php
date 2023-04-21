a5727002588d6faa73ee33b76ab450c7e7cf034f
Lisa <lisahakhoff@ziggo.nl>
2023-04-21 13:49:08 +0200

    Layout fixes - start of navbar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,24 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">  
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"> 
+    {{-- @vite('resources/js/script.js')  --}}
     @vite('resources/css/style.css')
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
     <title>{{ $title }} // {{ config('app.name')}}</title>
 </head>
 <body>
-    <div class="flex flex-col h-screen justify-between">
+    <div class="flex flex-col justify-between">
         <div class="header">
             @include('layout.navbar')
         </div>
 
-
-        <main class="mb-auto h-screen">
-            @yield('section') 
+        <main class="h-screen">
+            @yield('section')
         </main>
 
-        <footer class="h-14 bg-gray-300">
+        <footer class="h-24 bg-gray-300">
             @include('layout.footer')
         </footer>
     </div>
--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,37 +1,45 @@
+<header class="relative">
+  <!-- Logo -->
+    <div class="ml-4 flex lg:ml-0 justify-center">
+      <a href="#">
+        <span class="font-sans text-xl">Larimar</span>
+      </a>
+    </div>
+  <nav aria-label="Top" class="mx-auto max-w-auto">
+    <div class="border-b border-gray-200">      
+      <div class="flex h-16 items-center">
+        {{-- Home --}}
+        
 
-  <header class="relative bg-white">
-    <p class="flex h-10 items-center justify-center bg-indigo-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">Get free delivery on orders over $100</p>
-
-    <nav aria-label="Top" class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-      <div class="border-b border-gray-200">
-        <div class="flex h-16 items-center">
-          {{-- Login or Register --}}
-          <div class="ml-auto flex items-center">
-            <div class="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-              <a href="#" class="text-sm font-medium text-gray-700 hover:text-gray-800">Sign in</a>
-              <span class="h-6 w-px bg-gray-200" aria-hidden="true"></span>
-              <a href="#" class="text-sm font-medium text-gray-700 hover:text-gray-800">Create account</a>
-            </div>
-
+        {{-- Login or Register --}}
+        <div class="ml-auto mr-4 flex items-center">
+          <div class="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+            <a href="#" class="flex flex-col items-center text-sm font-medium text-gray-700 hover:text-gray-500">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+              </svg>
+              <div class="">Account</div>              
+            </a>
+            <span class="h-6 w-px bg-gray-200" aria-hidden="true"></span>
+        
             {{-- Wishlist --}}
             <div class="flex lg:ml-6">
-              <a href="#" class="p-2 text-gray-400 hover:text-gray-500">
-              <span class="sr-only">Search</span>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-16">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-              </svg>
+              <a href="#" class="flex flex-col items-center p-2 text-gray-700 hover:text-gray-500">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                </svg>
+                <div>Favorieten</div>  
               </a>
             </div>
-
+        
             {{-- Shopping cart --}}
-            <div class="ml-4 flow-root lg:ml-6">
-              <a href="#" class="group -m-2 flex items-center p-2">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-16">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-              </svg>
-              <span class="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
-              <span class="sr-only">Winkelmandje</span>
-              </a>
+            <div class="flex lg:ml-6">
+              <a href="#" class="flex flex-col items-center p-2 text-gray-700 hover:text-gray-500">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                </svg>
+              <div>Winkelmandje</div>  
+            </a>
             </div>
           </div>
         </div>
